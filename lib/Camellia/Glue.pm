4a3798f18fc77fe04f1ea8982c95d089e8acc21d
retrhelo <artyomliu@foxmail.com>
2022-08-08 22:54:53 +0800

    Define basic Glue member fields and new()

--- lib/Camellia/Glue.pm
+++ lib/Camellia/Glue.pm
@@ -6,4 +6,13 @@ require Exporter;
 our @ISA = qw(Exporter);
 our @EXPORT = qw();
 
+sub new {
+  my ($class, $args) = @_;
+  return bless {
+    name => $args->{name},    # name of top module
+    stack => [],              # contains instances and rawcode
+    wire => []                # connecting wires between instances
+  }, $class;
+}
+
 1;
